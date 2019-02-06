43486db132813f839375cff9d3f7919cb6ea1425
Suchit Puri <suchitpuri@suchitpuri-macbookpro.roam.corp.google.com>
2019-02-06 21:01:20 +0530

    get the dag using gcloud command line and python

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 gcloud config set project $PROJECT_ID
-gcloud projects list
+export DAG_LOCATION=`gcloud composer environments describe $COMPOSER --location=$LOCATION --format="json" | python3 -c "import sys, json; print(json.load(sys.stdin)['config']['dagGcsPrefix'])"`
+echo $DAG_LOCATION
+
 
