15acccd24aa95f332539c6df6ee504adce54a858
Suchit Puri <suchitpuri@suchitpuri-macbookpro.roam.corp.google.com>
2019-02-11 15:08:29 +0530

    using the public docker image hosted on gcr.io

--- examples/example_dag_factory.py
+++
@@ -1,7 +0,0 @@
-from airflow import DAG
-import dagfactory
-
-config_file = "/home/airflow/gcs/dags/example_dag_factory.yml"
-example_dag_factory = dagfactory.DagFactory(config_file)
-
-example_dag_factory.generate_dags(globals())
--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,6 +4,14 @@ read -p "project-id: " PROJECT_ID
 read -p "composer-id: " COMPOSER_ID
 read -p "composer location: " LOCATION
 read -p "absolute path of the YAML file ( leave it empty to use the sample yaml ): " YAML
+if [ -z "$YAML" ]
+then
+      echo "\Using sample YML file"
+      YAML="${pwd}/../examples/bq-dag.yml"
+fi
+
+
 FILENAME=$(basename $YAML)
 
-docker run -e PROJECT_ID=$PROJECT_ID -e COMPOSER=$COMPOSER_ID -e LOCATION=$LOCATION -e YAML=$YAML -e FILENAME=$FILENAME -it -v ~/.config/:/root/.config -v ${YAML}:/root/${FILENAME} f275f69cf18f
+
+docker run -e PROJECT_ID=$PROJECT_ID -e COMPOSER=$COMPOSER_ID -e LOCATION=$LOCATION -e YAML=$YAML -e FILENAME=$FILENAME -it -v ~/.config/:/root/.config -v ${YAML}:/root/${FILENAME} gcr.io/pso-suchit/auto-compose
